c978c9b98e31e4de80decc326443d2031962a7b6
liyanfeng <yanfeng.li@medishare.cn>
2019-06-12 13:39:37 +0800

    build ok

--- pose_graph_3d/pose_graph_3d.cc
+++ pose_graph_3d/pose_graph_3d.cc
@@ -5,8 +5,6 @@
 #include <string>
 
 #include <ceres/ceres.h>
-//#include <gflags/gflags.h>
-
 #include "common/read_g2o.h"
 #include "types.h"
 #include "pose_graph_3d_error_term.h"
@@ -33,11 +31,11 @@ void BuildOptimizationProblem(const VectorOfConstraints& constraints,
     const Constraint3d& constraint = *constraints_iter;
 
     MapOfPoses::iterator pose_begin_iter = poses->find(constraint.id_begin);
-    if (pose_begin_iter == poses->end())
-       std::cerr << "Pose with ID: " << constraint.id_begin << " not found.";
+    CHECK (pose_begin_iter != poses->end())
+        << "Pose with ID: " << constraint.id_begin << " not found.";
     MapOfPoses::iterator pose_end_iter = poses->find(constraint.id_end);
-    if (pose_end_iter == poses->end())
-       std::cerr << "Pose with ID: " << constraint.id_end << " not found.";
+    CHECK (pose_end_iter != poses->end())
+       << "Pose with ID: " << constraint.id_end << " not found.";
 
     const Eigen::Matrix<double, 6, 6> sqrt_information =
         constraint.information.llt().matrixL();
@@ -65,15 +63,93 @@ void BuildOptimizationProblem(const VectorOfConstraints& constraints,
   // constrain the gauge freedom. This can be done by setting one of the poses
   // as constant so the optimizer cannot change it.
   MapOfPoses::iterator pose_start_iter = poses->begin();
-  if (pose_start_iter == poses->end()) std::cerr << "There are no poses.";
+  CHECK (pose_start_iter != poses->end()) << "There are no poses.";
   problem->SetParameterBlockConstant(pose_start_iter->second.p.data());
   problem->SetParameterBlockConstant(pose_start_iter->second.q.coeffs().data());
+
+  std::cout << "build problem end" << std::endl;
 }
+
+
+// Returns true if the solve was successful.
+bool SolveOptimizationProblem(ceres::Problem* problem) {
+  CHECK(problem != NULL);
+
+  ceres::Solver::Options options;
+  options.max_num_iterations = 200;
+  options.linear_solver_type = ceres::SPARSE_NORMAL_CHOLESKY;
+
+  ceres::Solver::Summary summary;
+  ceres::Solve(options, problem, &summary);
+
+  std::cout << summary.FullReport() << '\n';
+
+  return summary.IsSolutionUsable();
+}
+
+// Output the poses to the file with format: id x y z q_x q_y q_z q_w.
+bool OutputPoses(const std::string& filename, const MapOfPoses& poses) {
+  std::fstream outfile;
+  outfile.open(filename.c_str(), std::istream::out);
+  if (!outfile) {
+    LOG(ERROR) << "Error opening the file: " << filename;
+    return false;
+  }
+  for (std::map<int, Pose3d, std::less<int>,
+                Eigen::aligned_allocator<std::pair<const int, Pose3d> > >::
+           const_iterator poses_iter = poses.begin();
+       poses_iter != poses.end(); ++poses_iter) {
+    const std::map<int, Pose3d, std::less<int>,
+                   Eigen::aligned_allocator<std::pair<const int, Pose3d> > >::
+        value_type& pair = *poses_iter;
+    outfile << pair.first << " " << pair.second.p.transpose() << " "
+            << pair.second.q.x() << " " << pair.second.q.y() << " "
+            << pair.second.q.z() << " " << pair.second.q.w() << '\n';
+  }
+  return true;
+}
+
 
 } // namespace mytest
 
 int main(int argc, char** argv) {
-  Eigen::Matrix<double, 6, 6> aa;
-  std::cout << aa;
+  
+  google::InitGoogleLogging(argv[0]);
+  //CERES_GFLAGS_NAMESPACE::ParseCommandLineFlags(&argc, &argv, true);
+
+  std::string FLAGS_input = argv[1];
+  if(FLAGS_input == "") std::cerr << "Need to specify the filename to read.";
+
+  mytest::MapOfPoses poses;
+  mytest::VectorOfConstraints constraints;
+
+  if (!mytest::ReadG2oFile(FLAGS_input, &poses, &constraints)){
+
+    std::cerr << "Error reading the file: " << FLAGS_input;
+    return 1;
+  }
+
+  std::cout << "Number of poses: " << poses.size() << '\n';
+  std::cout << "Number of constraints: " << constraints.size() << '\n';
+
+  if (!mytest::OutputPoses("poses_original.txt", poses)) { 
+      std::cerr << "Error outputting to poses_original.txt";
+      return 1;
+  }
+
+  ceres::Problem problem;
+  mytest::BuildOptimizationProblem(constraints, &poses, &problem);
+
+  std::cout << "build problem end" << std::endl;
+
+  if (!mytest::SolveOptimizationProblem(&problem)) {
+      std::cerr << "The solve was not successful, exiting." << std::endl;
+  }
+  
+  std::cout << "solve problem end" << std::endl;
+
+  if (!mytest::OutputPoses("poses_optimized.txt", poses))
+     std::cerr << "Error outputting to poses_original.txt" << std::endl;
+
   return 0;
 }
---
+++ show.sh
@@ -0,0 +1,2 @@
+python3 ./pose_graph_3d/plot_results.py --optimized_poses ./poses_optimized.txt --initial_poses ./poses_original.txt
+
